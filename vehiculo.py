40af28e9ae7af44b848075d2cdb5eb1854663f5d
panchoriqfar <tallerespa@inacapmail.cl>
2026-09-07 21:28:49 -0300

    feat: agregar clase Vehiculo en vehiculo.py

---
+++ vehiculo.py
@@ -0,0 +1,7 @@
+# Se define la clase Vehiculo utilizando la palabra reservada 'class' (en Python se recomienda usar PascalCase)
+class Vehiculo:
+    # La palabra reservada 'pass' sirve como marcador de posición para definir la clase vacía sin generar errores de sintaxis
+    pass
+
+# Se define un alias en minúsculas para compatibilidad si se requiere llamar a la clase como 'vehiculo'
+vehiculo = Vehiculo
