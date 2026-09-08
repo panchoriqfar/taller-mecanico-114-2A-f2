d1917eeed49e71798131c21867ddb045309c10e5
panchoriqfar <tallerespa@inacapmail.cl>
2026-09-07 22:02:48 -0300

    feat: implementar constructor __init__ en la clase Vehiculo

--- vehiculo.py
+++ vehiculo.py
@@ -1,8 +1,10 @@
-# Se define la clase Vehiculo utilizando la palabra reservada 'class' (en Python se recomienda usar PascalCase)
+# Se define la clase Vehiculo utilizando la convención estándar PascalCase
 class Vehiculo:
-    # Declaración del atributo 'patente' de tipo texto (cadena de caracteres)
-    patente: str
-    # Declaración del atributo 'annio' de tipo número entero
-    annio: int
-    # Declaración del atributo 'en_taller' de tipo booleano (verdadero o falso)
-    en_taller: bool
+    # Método constructor que inicializa los atributos del vehículo al instanciar la clase
+    def __init__(self, patente: str, annio: int, en_taller: bool = False):
+        # Se asigna la patente (tipo texto) al atributo de instancia del objeto
+        self.patente: str = patente
+        # Se asigna el año de fabricación (tipo entero) al atributo de instancia del objeto
+        self.annio: int = annio
+        # Se asigna el estado en taller (tipo booleano, por defecto False) al atributo de instancia
+        self.en_taller: bool = en_taller
