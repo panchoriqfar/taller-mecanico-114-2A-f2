9c79f2c471f2dd3b5e5a7fa5ebf2aa5aac980d92
panchoriqfar <tallerespa@inacapmail.cl>
2026-09-07 21:39:49 -0300

    feat: declarar atributos patente, annio y en_taller en Vehiculo

--- vehiculo.py
+++ vehiculo.py
@@ -1,7 +1,11 @@
 # Se define la clase Vehiculo utilizando la palabra reservada 'class' (en Python se recomienda usar PascalCase)
 class Vehiculo:
-    # La palabra reservada 'pass' sirve como marcador de posición para definir la clase vacía sin generar errores de sintaxis
-    pass
+    # Declaración del atributo 'patente' de tipo texto (cadena de caracteres)
+    patente: str
+    # Declaración del atributo 'annio' de tipo número entero
+    annio: int
+    # Declaración del atributo 'en_taller' de tipo booleano (verdadero o falso)
+    en_taller: bool
 
 # Se define un alias en minúsculas para compatibilidad si se requiere llamar a la clase como 'vehiculo'
 vehiculo = Vehiculo
